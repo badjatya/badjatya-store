48975b32cc71c053233923d2ddd7f3cfaf8bce03
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-12-05 12:04:15 +0530

    Database connected

---
+++ src/database/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+exports.dbConnect = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("DATABASE CONNECTED"))
+    .catch((error) => {
+      console.log("DATABASE NOT CONNECTED");
+      console.log(error);
+      process.exit(1);
+    });
+};
