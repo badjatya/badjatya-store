64e61595ddcbea7454c51016c1e61c63f358c2e1
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-12-05 13:40:48 +0530

    swagger documentaion added

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ const express = require("express");
 require("dotenv").config({ path: "./src/config/config.env" });
 const app = express();
 
+// Library
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+// API DOCS Swagger
+const swaggerDocument = YAML.load("./src/docs/swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Exporting app
 module.exports = app;
