7148475b9779db9871fe77e1274356a433f89412
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-12-06 23:26:49 +0530

    Creating user and sending verification email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 // Model
 const User = require("../models/user");
 
+// Library
+const cloudinary = require("cloudinary");
+
 // Utils
 const emailSender = require("../utils/emailSender");
 const customError = require("../utils/customError");
@@ -20,31 +23,74 @@ exports.createUser = async (req, res) => {
       return customError(res, 401, "User already exists, please login");
     }
 
-    // Creating new user
-    const user = await User.create({ name, email, password });
+    // If user has uploaded an image
+    if (req.files) {
+      // Uploading image to cloudinary
+      const result = await cloudinary.v2.uploader.upload(
+        req.files.photo.tempFilePath,
+        {
+          folder: "badjatya-store/users",
+          width: 150,
+          crop: "scale",
+        }
+      );
+      const photo = {
+        secureUrl: result.secure_url,
+        publicId: result.public_id,
+      };
 
-    // confirm email token (valid for 20min)
-    const confirmEmailToken = user.getJwtConfirmEmailToken();
+      // Creating new user
+      const user = await User.create({ name, email, password, photo });
 
-    // Url for token
-    const tokenUrl = `${req.protocol}://${req.get(
-      "host"
-    )}/api/v1/users/email/confirm/${confirmEmailToken}`;
+      // confirm email token (valid for 20min)
+      const confirmEmailToken = user.getJwtConfirmEmailToken();
 
-    emailSender({
-      email,
-      subject: `Confirm email mail to ${name}`,
-      text: "Click on the button to confirm email",
-      html: `
-      <p style="margin-bottom: 20px">Click below button to confirm email </p>
+      // Url for token
+      const tokenUrl = `${req.protocol}://${req.get(
+        "host"
+      )}/api/v1/users/email/confirm/${confirmEmailToken}`;
+
+      emailSender({
+        email,
+        subject: `Confirm email mail to ${name}`,
+        text: "Click on the button to confirm email",
+        html: `
+      <p style="margin-bottom: 20px">Hey ${name}, Click below button to confirm email </p>
       <a style="display:block; text-align:center; padding:20px; background-color: black; color: white; border-radius: 30px; text-decoration:none " href=${tokenUrl}>Confirm email</a>
       `,
-    });
+      });
 
-    res.json({
-      status: "success",
-      message: "Email sent successfully, confirm email",
-    });
+      res.json({
+        status: "success",
+        message: "Email sent successfully, confirm email",
+      });
+    } else {
+      // Creating new user
+      const user = await User.create({ name, email, password });
+
+      // confirm email token (valid for 20min)
+      const confirmEmailToken = user.getJwtConfirmEmailToken();
+
+      // Url for token
+      const tokenUrl = `${req.protocol}://${req.get(
+        "host"
+      )}/api/v1/users/email/confirm/${confirmEmailToken}`;
+
+      emailSender({
+        email,
+        subject: `Confirm email mail to ${name}`,
+        text: "Click on the button to confirm email",
+        html: `
+          <p style="margin-bottom: 20px">Hey ${name}, Click below button to confirm email </p>
+          <a style="display:block; text-align:center; padding:20px; background-color: black; color: white; border-radius: 30px; text-decoration:none " href=${tokenUrl}>Confirm email</a>
+          `,
+      });
+
+      res.json({
+        status: "success",
+        message: "Email sent successfully, confirm email",
+      });
+    }
   } catch (error) {
     customError(res, 500, error.message, "error");
   }
